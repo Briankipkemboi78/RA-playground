fd438c634b98afbbe819d4e27f177612537a36e7
Brian Kipkemboi <briankipkemboi47@gmail.com>
2025-05-30 12:37:23 +0200

    Adding other necessary columns

--- Revenue_tracker.sql
+++ Revenue_tracker.sql
@@ -26,6 +26,8 @@ SELECT
 	footprint.standard_mt_volume,
 	footprint.standard_mt_equivalent_volume,
 	footprint.farmch_sale_flag,
+	trans_detail.transaction_type,
+	trans_detail.transaction_status,
 	product.crop,
 	product.crop_category,
 	product.royalty_method,
@@ -33,7 +35,6 @@ SELECT
 	cert_holder.reporting_certificateholder_name                                              AS cert_name,
 	cert_holder.reporting_certificateholder_country                                           AS certificate_holder_country,
 	license.license_volume                                                                    AS license_volume,
-	license.equivalent_volume_license_mt,
 	license.carry_over_volume,
 	licence_year.license_status,
 	licence_year.start_date_license,
@@ -43,7 +44,7 @@ LEFT JOIN prd.fact_transaction AS trans ON
   footprint.dim_transactiondetail_id = trans.dim_transactiondetail_id AND
   trans.source_transaction_id = footprint.source_transaction_id
 LEFT JOIN prd.dim_transactiondetail AS trans_detail ON
-  trans.dim_transactiondetail_id = trans_detail.
+  trans.dim_transactiondetail_id = trans_detail.dim_transactiondetail_id
 LEFT JOIN prd.dim_subproduct AS product ON
 	footprint.dim_subproduct_id = product.dim_subproduct_id
 LEFT JOIN prd.dim_reporting_certificateholder AS cert_holder ON
@@ -65,5 +66,3 @@ WHERE
 ORDER BY 
     footprint.source_transaction_id
 
-	
-	SELECT * FROM prd.dim_transactiondetail
