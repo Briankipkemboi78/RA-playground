4b0c37eded0a1bf9ad1481830db9a134d617dec9
Brian Kipkemboi <briankipkemboi47@gmail.com>
2025-05-30 11:06:59 +0200

    Adding more features to assist in analysis

--- Revenue_tracker.sql
+++ Revenue_tracker.sql
@@ -17,6 +17,7 @@ SELECT
 	footprint.scheme_owner,
 	footprint.original_scheme_owner,
 	footprint.original_volume                                                                 AS original_volume,
+	footprint.standard_mt_equivalent_volume                                                   AS equivalent_mt_volume,
 	trans.royalty_applied, 
 	trans.royalty_calculated,
 	trans.royalty_fee_rate,
@@ -32,19 +33,23 @@ SELECT
 	cert_holder.reporting_certificateholder_name                                              AS cert_name,
 	cert_holder.reporting_certificateholder_country                                           AS certificate_holder_country,
 	license.license_volume                                                                    AS license_volume,
+	license.equivalent_volume_license_mt,
 	license.carry_over_volume,
+	licence_year.license_status,
 	licence_year.start_date_license,
 	YEAR(licence_year.start_date_license) AS license_year
 FROM prd.fact_transactionfootprint AS footprint
 LEFT JOIN prd.fact_transaction AS trans ON
   footprint.dim_transactiondetail_id = trans.dim_transactiondetail_id AND
   trans.source_transaction_id = footprint.source_transaction_id
+LEFT JOIN prd.dim_transactiondetail AS trans_detail ON
+  trans.dim_transactiondetail_id = trans_detail.
 LEFT JOIN prd.dim_subproduct AS product ON
 	footprint.dim_subproduct_id = product.dim_subproduct_id
 LEFT JOIN prd.dim_reporting_certificateholder AS cert_holder ON
 	footprint.dim_reporting_certificateholder_id = cert_holder.dim_reporting_certificateholder_id
 LEFT JOIN prd.dim_license AS licence_year ON  
-	footprint.dim_footprint_certificateholder_id = licence_year.dim_certificateholder_id
+	footprint.dim_footprint_certificateholder_id = licence_year.dim_certificateholder_id 
 LEFT JOIN prd.dim_certificateholder AS cert_owner ON  
 	cert_holder.dim_reporting_certificateholder_id = cert_owner.dim_certificateholder_id
 LEFT JOIN prd.fact_licensevolume AS license ON  
@@ -59,3 +64,6 @@ WHERE
 	--footprint.source_transaction_id = '1000618'
 ORDER BY 
     footprint.source_transaction_id
+
+	
+	SELECT * FROM prd.dim_transactiondetail
